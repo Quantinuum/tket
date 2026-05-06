275465c9d51772766e26a4e53bbdea908dd78ea6
Melf <cqc@melf.de>
2026-05-06 21:08:48 +0100

    update tests

--- pytket/tests/classical_test.py
+++ pytket/tests/classical_test.py
@@ -924,6 +924,9 @@ def test_wasm_box() -> None:
 
     DecomposeBoxes().apply(c1)
 
+    assert c1.depth() == 1
+    assert str(c1.get_commands()[0]) == "WASM A[0], B[0], _w[0];"
+
 
 def test_rng_box() -> None:
 
@@ -938,7 +941,10 @@ def test_rng_box() -> None:
     c1.add_c_register(A)
     c1.add_circbox_regwise(c0box, qregs=[], cregs=[A])
 
-    DecomposeBoxes().apply(c1) # melf
+    DecomposeBoxes().apply(c1)
+
+    assert c1.depth() == 1
+    assert str(c1.get_commands()[0]) == "RNGNum A[0], A[1], A[2], A[3], A[4], A[5], A[6], A[7], A[8], A[9], A[10], A[11], A[12], A[13], A[14], A[15], A[16], A[17], A[18], A[19], A[20], A[21], A[22], A[23], A[24], A[25], A[26], A[27], A[28], A[29], A[30], A[31], _r[0];"
 
 
 @strategies.composite
